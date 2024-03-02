587c80ae7287ab9b90c6d9561a7e39ea63b47e65
uuuta714 <horiyuuu@gmail.com>
2024-03-02 22:14:34 +1100

    Built a walking skeleton for API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+        private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+            _context = context;
+
+    }
+
+    [HttpGet]
+    // Define what to return from this endpoint
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")] // /api/users/e.g.2(id)
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    //Properties has to be public to use Entity Framework
+    public int Id { get; set; }
+    public string UserName {get; set;}
+}
