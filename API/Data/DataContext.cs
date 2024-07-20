9a258081d2e5a7dfe32b99c545f66f23350a75fc
uuuta714 <horiyuuu@gmail.com>
2024-07-21 00:51:09 +1000

    Walking skeleton for API

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    // Name of a table in the database
+    public DbSet<AppUser> Users { get; set; }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    // Access modifier should be public to use Enity Framework 
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+}
