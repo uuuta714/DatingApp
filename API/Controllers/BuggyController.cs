ec89cebabd6bef1fa982be71d4d1d3eaea103c03
uuuta714 <horiyuuu@gmail.com>
2024-07-29 23:07:31 +1000

    Global Error Handling in both the API and the Angular application

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,48 @@
+﻿using API.Controllers;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+public class BuggyController(DataContext context) : BaseApiController
+{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secret text";
+    }
+    // By testing with {{url}}/api/buggy/auth, it will return 401 Unauthorized
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+        var thing = context.Users.Find(-1);
+        if (thing == null) return NotFound();
+        return thing;
+    }
+    // By testing with {{url}}/api/buggy/not-found, it will return a body includes;
+    //{
+    //  "type": "https://tools.ietf.org/html/rfc9110#section-15.5.5",
+    //  "title": "Not Found",
+    //  "status": 404,
+    //  "traceId": "00-4e8ed3e8f6c5aa8e36b86d7e78553ed9-3c764d9e19960c2a-00"
+    //}
+
+    [HttpGet("server-error")]
+    public ActionResult<AppUser> GetServerError()
+    {
+        var thing = context.Users.Find(-1) ?? throw new Exception("A bad thing has happened");
+        return thing;
+    }
+    // By testing with {{url}}/api/buggy/server-error, exception message will be displayed
+
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("This was not a good request");
+    }
+    // By testing with {{url}}/api/buggy/bad-request, it will return a message 'This is not a good request'
+}
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace API;
+
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+{
+    // next() will look for InvokeAsync(), do not change the name of the method
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            // Simply move on to the next request delegate
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            // Log out to the terminal
+            logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = env.IsDevelopment()
+                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                : new ApiException(context.Response.StatusCode, ex.Message, "Internal server error");
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            // Provide response as json response so that we can work on the angular client side
+            var json = JsonSerializer.Serialize(response, options);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
